51c68e300f92cde346bd2ceca836d3ed37fbbd76
Yugantar Jain <yugantar8jain@gmail.com>
2019-08-28 11:59:19 +0530

    Initial Commit

---
+++ Synd Customer/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Synd Customer
+//
+//  Created by yugantar jain on 28/08/19.
+//  Copyright © 2019 yugantar jain. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Synd Customer/ScanQRViewController.swift
@@ -0,0 +1,75 @@
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var captureSession = AVCaptureSession()
+    var videoPreviewLayer = AVCaptureVideoPreviewLayer()
+    
+    @IBOutlet weak var scanlabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized: // The user has previously granted access to the camera.
+            self.setupCaptureSession()
+            
+        case .notDetermined: // The user has not yet been asked for camera access.
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted {
+                    self.setupCaptureSession()
+                }
+            }
+            
+        case .denied: // The user has previously denied access.
+            return
+        case .restricted: // The user can't grant access due to restrictions.
+            return
+        default:
+            return
+        }
+        
+        view.bringSubviewToFront(scanlabel)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //navigationController?.isNavigationBarHidden = true
+        captureSession.startRunning()
+    }
+    
+    func setupCaptureSession()
+    {
+        captureSession.beginConfiguration()
+        
+        let videoDevice = AVCaptureDevice.default(for: .video)
+        
+        guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!),
+            captureSession.canAddInput(videoDeviceInput) else { return }
+        captureSession.addInput(videoDeviceInput)
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        guard captureSession.canAddOutput(metadataOutput) else { return }
+        captureSession.addOutput(metadataOutput)
+        
+        captureSession.commitConfiguration()
+        
+        metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        metadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.frame = self.view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer)
+        
+        captureSession.startRunning()
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        guard let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject else {
+            return
+        }
+        self.scanlabel.text = "Syndicate bank ****** QR code detected. \nYou are being checked in automatically"
+        //        captureSession.stopRunning()
+    }
+}
