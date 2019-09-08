b8746ab89ab78de9c921098f4a0f146ed8e5a8d6
Yugantar Jain <yugantar8jain@gmail.com>
2019-09-08 18:57:30 +0530

    ok

--- Synd Customer/AppDelegate.swift
+++ Synd Customer/AppDelegate.swift
@@ -7,7 +7,8 @@
 //
 
 import UIKit
-//import Firebase
+import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-//        FirebaseApp.configure()
+        FirebaseApp.configure()
         return true
     }
 
---
+++ Synd Customer/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  Synd Customer
+//
+//  Created by yugantar jain on 08/09/19.
+//  Copyright © 2019 yugantar jain. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import GoogleSignIn
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
