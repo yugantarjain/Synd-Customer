78a2ce6431bcf8f40dbf9f60ebc7a712945238b9
Yugantar Jain <yugantar8jain@gmail.com>
2019-09-08 23:41:37 +0530

    stable

--- Synd Customer/CustomerQueueViewController.swift
+++ Synd Customer/CustomerQueueViewController.swift
@@ -7,47 +7,74 @@
 //
 
 import UIKit
-import Starscream
-import Foundation
+import FirebaseAuth
 
-class CustomerQueueViewController: UIViewController, WebSocketDelegate {
+struct DataUser: Decodable {
+    let message: Int?
+}
+
+class CustomerQueueViewController: UIViewController {
     
-    var socket: WebSocket!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        socket = WebSocket.init(url: URL(string: "https://hack321.herokuapp.com")!)
-        socket.delegate = self
-        socket.connect()
-    }
-    
-    func websocketDidConnect(socket: WebSocketClient) {
-        print("connect")
-        return
-    }
-    
-    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
-        print("disconnect")
-        return
-    }
-    
-    func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
-        print("string")
-        return
-    }
-    
-    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
-        print("data")
-        return
+        welcomeLabel.text = "Welcome, \((Auth.auth().currentUser?.displayName)!)"
+        
+        getPosition()
     }
     
     @IBOutlet weak var queueNumber: UILabel!
     @IBOutlet weak var queueNumberMessage: UILabel!
     @IBOutlet weak var timeMessage: UILabel!
     @IBOutlet weak var counterMessage: UILabel!
+    @IBOutlet weak var welcomeLabel: UILabel!
     
-
+    func getPosition() {
+        let urlString = "https://hack321.herokuapp.com/position"
+        let url = URL(string: urlString)
+        var request = URLRequest.init(url: url!)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        let email = Auth.auth().currentUser?.email
+        let user = User.init(email: email)
+        guard let uploadData = try? JSONEncoder().encode(user) else { return }
+        
+        URLSession.shared.uploadTask(with: request, from: uploadData) { (data, response, error) in
+            guard error == nil else { return }
+            guard let response = response as? HTTPURLResponse,
+                (200...299).contains(response.statusCode) else {
+                    print ("server error")
+                    return
+            }
+            //api success
+            guard let json = try? JSONDecoder().decode(DataUser.self, from: data!) else {
+                DispatchQueue.main.async {
+                    self.queueNumber.text = "-"
+                    self.queueNumberMessage.text = "Currently, there are - people ahead of you in the queue"
+                    self.timeMessage.text = "Your turn should come within - minutes"
+                    self.counterMessage.text = "On arrival of your turn, kindly proceed towards counter number -"
+                }
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 5, execute: {
+                    self.navigationController?.popToRootViewController(animated: true)
+                })
+                return
+            }
+            DispatchQueue.main.async {
+                let number = json.message!
+                self.queueNumber.text = String(number)
+                self.queueNumberMessage.text = "Currently, there are \(number) people ahead of you in the queue"
+                self.timeMessage.text = "Your turn should come within \(number*7) minutes"
+                self.counterMessage.text = "On arrival of your turn, kindly proceed towards counter number \(number/2 + 1)"
+            }
+        }.resume()
+    }
+    
+    @IBAction func refersh(_ sender: Any) {
+        getPosition()
+    }
+    
     /*
     // MARK: - Navigation
 
